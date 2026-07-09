f76859cab1e0c36946b5c6ba081e5759c8822769
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-09 16:52:04 +0200

    update files

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -5,22 +5,43 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
 // Part 2: Even / Odd
 const number = 6;
 
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
+if (numberOfHotdogs < 5) {
+  console.log("2 Euro");
+} else if (numberOfHotdogs < 100) {
+  console.log("1.50 Euro");
+} else if (numberOfHotdogs < 1000000) {
+  console.log("1 Euro");
+} else {
+  console.log("0.10 Euro");
+}
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 17;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn..." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
 const userName = "Archibald";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (userName === "Feline" ? "Coach" : userName) + "!";
 
 console.log(greeting);
