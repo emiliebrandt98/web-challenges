fc5b8066e9247fb67d31aa7b49728b5479490732
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-09 17:23:36 +0200

    update files

--- js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans_weather-check/js/index.js
@@ -1,40 +1,42 @@
 // Change this value to test different "weather" conditions.
-// const weather = "rainy";
-// switch (weather) {
-//   case "sunny":
-//     console.log("It's sunny!");
-//     break;
-
-//   case "rainy":
-//     console.log("It's raining, don't forget your umbrella!");
-//     break;
-
-//   case "snowy":
-//     console.log("It's snowing, stay warm!");
-//     break;
-
-//   default:
-//     console.log("I'm not sure what the weather is like.");
-// }
-
-// Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22;
-const weather = "rainy";
-switch (true) {
-  case weather === "sunny" && temperature > 20:
-    console.log("It's sunny and warm!");
+const weather = "snowy";
+switch (weather) {
+  case "sunny":
+    console.log("It's sunny!");
     break;
 
-  case weather === "sunny" && temperature <= 20:
-    console.log("It's sunny, but a bit chilly!");
-    break;
-
-  case weather === "rainy":
+  case "rainy":
     console.log("It's raining, don't forget your umbrella!");
     break;
 
-  case weather === "snowy" && temperature < 0:
-    console.log("Freezing snow!");
+  case "snowy":
+    console.log("It's snowing, stay warm!");
+    break;
+
+  default:
+    console.log("I'm not sure what the weather is like.");
+}
+
+// Adjust the "temperature" value to trigger different messages (for Part 2 only).
+const temperature = -3;
+
+switch (weather) {
+  case "sunny":
+    if (temperature > 20) {
+      console.log("It's sunny and warm!");
+    } else {
+      console.log("It's sunny, but a bit chilly!");
+    }
+    break;
+
+  case "rainy":
+    console.log("It's raining, don't forget your umbrella!");
+    break;
+
+  case "snowy":
+    if (temperature < 0) {
+      console.log("Freezing snow!");
+    }
     break;
 
   default:
