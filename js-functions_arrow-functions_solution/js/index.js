31b34c06bd160361a983154b63d13492756e3479
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-10 18:22:48 +0200

    Update files

--- js-functions_arrow-functions_solution/js/index.js
+++ js-functions_arrow-functions_solution/js/index.js
@@ -7,19 +7,21 @@ Use implicit returns when possible.
 HINT: You can find more information about arrow functions and implicit returns in the handout.
 */
 
-// function getCurrentHour() {
-//   const now = new Date();
-//   const currentHour = now.getHours();
-//   if (currentHour === 0) {
-//     return "12am";
-//   } else if (currentHour === 12) {
-//     return "12pm";
-//   } else if (currentHour <= 11) {
-//     return currentHour + "am";
-//   } else {
-//     return currentHour - 12 + "pm";
-//   }
-// }
+/*
+function getCurrentHour() {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return '12am';
+  } else if (currentHour === 12) {
+    return '12pm';
+  } else if (currentHour <= 11) {
+    return currentHour + 'am';
+  } else {
+    return currentHour - 12 + 'pm';
+  }
+}
+*/
 
 const getCurrentHour = () => {
   const now = new Date();
@@ -34,37 +36,47 @@ const getCurrentHour = () => {
     return currentHour - 12 + "pm";
   }
 };
+
 console.log("Get current hour: ", getCurrentHour());
 
-// function getVectorLength(x, y, z) {
-//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-// }
+/*
+function getVectorLength(x, y, z) {
+  return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+}
+*/
 
 const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-console.log("Get vector lenght: ", getVectorLength(2, 3, 4));
 
-// function cleanInput(string) {
-//   return string.toLowerCase().trim();
-// }
+console.log("Get vector length: ", getVectorLength(2, 3, 4));
+
+/*
+function cleanInput(string) {
+  return string.toLowerCase().trim();
+}
+*/
 
 const cleanInput = (string) => string.toLowerCase().trim();
-console.log("Clean Input: ", cleanInput("Hello"));
+
+console.log("Clean input: ", cleanInput("       Hello World!      "));
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-// const isOddAndSmall = (number) => {
-//   if (number > 10) {
-//     return false;
-//   }
+/*
+const isOddAndSmall = number => {
+  if (number > 10) {
+    return false;
+  }
 
-//   if (number % 2 === 0) {
-//     return false;
-//   }
+  if (number % 2 === 0) {
+    return false;
+  }
 
-//   return true;
-// };
+  return true;
+};
+*/
+
 function isOddAndSmall(number) {
   if (number > 10) {
     return false;
@@ -76,16 +88,21 @@ function isOddAndSmall(number) {
 
   return true;
 }
-console.log("Is odd and small: " + isOddAndSmall(3));
+
+console.log("Is odd and small: ", isOddAndSmall(7));
 
 // const add3 = (a, b, c) => a + b + c;
+
 function add3(a, b, c) {
   return a + b + c;
 }
-console.log("Add 3: " + add3(1, 2, 3));
 
-// const repeat10 = (string) => string.repeat(10);
+console.log("Add three: ", add3(7, 8, 9));
+
+//const repeat10 = string => string.repeat(10);
+
 function repeat10(string) {
   return string.repeat(10);
 }
-console.log("Repeat ten: " + repeat10("-"));
+
+console.log("Repeat ten: ", repeat10("J"));
