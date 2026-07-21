7331047d2e2697e681b13972ece13015bb76db91
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-21 12:55:46 +0200

    finish challenges

--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -1,23 +1,19 @@
 console.clear();
 
+import { getRandomColor } from "./utils/randomColor.js";
+import { Circle } from "./components/Circle/Circle.js";
+import { Pentagon } from "./components/Pentagon/Pentagon.js";
+import { Square } from "./components/Square/Square.js";
+
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
-});
+// Circle
+const circleElement = Circle();
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
-});
+// Square
+const squareElement = Square();
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
-});
+// Pentagon
+const pentagonElement = Pentagon();
 
-root.append(circle, square, pentagon);
+root.append(circleElement, squareElement, pentagonElement);
