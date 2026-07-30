1be52258e7e74a50a86f7ee235690c1f1a4ff586
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-30 15:59:59 +0200

    finis challenges react nesting

--- react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>button 01</Button>
+      <Button>button 02</Button>
+      <Button>button 03</Button>
+      <Button>button 04</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
