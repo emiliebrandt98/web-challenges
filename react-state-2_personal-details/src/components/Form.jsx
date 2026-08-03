d927c418a158b79b8aeba01e1ac19bb474889fa8
Emilie Brandt <emiliebrandt@gmx.de>
2026-08-03 11:50:36 +0200

    finish challenges react state 2

--- react-state-2_personal-details/src/components/Form.jsx
+++ react-state-2_personal-details/src/components/Form.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
-
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form({ onCreateUser }) {
   function handleSubmit(event) {
     event.preventDefault();
+
+    const name = event.target.elements.name.value;
+    const email = event.target.elements.email.value;
+
+    onCreateUser(name, email);
+
+    event.target.reset();
+    event.target.elements.name.focus();
   }
 
   return (
