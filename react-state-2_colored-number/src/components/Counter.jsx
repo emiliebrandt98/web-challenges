efa22a7016911a62f76d109106191387056ee53c
Emilie Brandt <emiliebrandt@gmx.de>
2026-08-03 12:56:03 +0200

    update challenges react state 2

--- react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2_colored-number/src/components/Counter.jsx
@@ -4,13 +4,6 @@ import ColoredNumber from "./ColoredNumber";
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  function handleIncreaseCount() {
-    setCount(count + 1);
-  }
-  function handleDecreaseCount() {
-    setCount(count - 1);
-  }
-
   return (
     <div className="counter">
       <h1>Colored Number</h1>
@@ -20,7 +13,9 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment count"
-          onClick={handleIncreaseCount}
+          onClick={() => {
+            setCount(count + 1);
+          }}
         >
           +
         </button>
@@ -28,7 +23,9 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
-          onClick={handleDecreaseCount}
+          onClick={() => {
+            setCount(count - 1);
+          }}
         >
           -
         </button>
